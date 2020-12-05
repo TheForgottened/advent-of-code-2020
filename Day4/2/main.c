1f479a68f24a897ca9a4452820f54cab50483373
TheForgotten <angelo.mf.paiva@gmail.com>
2020-12-05 04:59:11 +0000

    Cleaning the repo up

--- Day4/2/main.c
+++ Day4/2/main.c
@@ -18,50 +18,45 @@ BOOL checkField(char* const fieldName, char* const fieldData) {
         return 1;
 
     int temp, i;
-    char fData[strlen(fieldData)];
-
-    strcpy(fData, fieldData);
-
-    printf("\nCHECKFIELD: %s, %s - %i", fieldName, fData, strlen(fData));
 
     if (strcmp(fieldName, "byr") == 0) {
-        temp = atoi(fData);
+        temp = atoi(fieldData);
         return (1920 <= temp && temp <= 2002 ? 1 : 0);
     } else if (strcmp(fieldName, "iyr") == 0) {
-        temp = atoi(fData);
+        temp = atoi(fieldData);
         return (2010 <= temp && temp <= 2020 ? 1 : 0);
     } else if (strcmp(fieldName, "eyr") == 0) {
-        temp = atoi(fData);
+        temp = atoi(fieldData);
         return (2020 <= temp && temp <= 2030 ? 1 : 0);
     } else if (strcmp(fieldName, "hgt") == 0) {
-        if (strlen(fData) == 5 && fData[strlen(fData) - 2] == 'c' && fData[strlen(fData) - 1] == 'm') {
-            temp = ((fData[0] - '0') * 100) + ((fData[1] - '0') * 10) + (fData[2] - '0');
+        if (strlen(fieldData) == 5 && fieldData[strlen(fieldData) - 2] == 'c' && fieldData[strlen(fieldData) - 1] == 'm') {
+            temp = ((fieldData[0] - '0') * 100) + ((fieldData[1] - '0') * 10) + (fieldData[2] - '0');
 
             return (150 <= temp && temp <= 193 ? 1 : 0);
-        } else if (strlen(fData) == 4 && fData[strlen(fData) - 2] == 'i' && fData[strlen(fData) - 1] == 'n') {
-            temp = ((fData[0] - '0') * 10) + (fData[1] - '0');
+        } else if (strlen(fieldData) == 4 && fieldData[strlen(fieldData) - 2] == 'i' && fieldData[strlen(fieldData) - 1] == 'n') {
+            temp = ((fieldData[0] - '0') * 10) + (fieldData[1] - '0');
 
             return (59 <= temp && temp <= 76 ? 1 : 0);
         } else
             return 0;
     } else if (strcmp(fieldName, "hcl") == 0) {
-        if (fData[0] != '#' || strlen(fData) != 7)
+        if (fieldData[0] != '#' || strlen(fieldData) != 7)
             return 0;
 
-        for (i = 1; i < strlen(fData); i++)
-            if (!isxdigit(fData[i]))
+        for (i = 1; i < strlen(fieldData); i++)
+            if (!isxdigit(fieldData[i]))
                 return 0;
 
         return 1;
     } else if (strcmp(fieldName, "ecl") == 0) {
-        temp = (strcmp(fData, "amb") == 0 || strcmp(fData, "blu") == 0
-             || strcmp(fData, "brn") == 0 || strcmp(fData, "gry") == 0
-             || strcmp(fData, "grn") == 0 || strcmp(fData, "hzl") == 0
-             || strcmp(fData, "oth") == 0);
+        temp = (strcmp(fieldData, "amb") == 0 || strcmp(fieldData, "blu") == 0
+             || strcmp(fieldData, "brn") == 0 || strcmp(fieldData, "gry") == 0
+             || strcmp(fieldData, "grn") == 0 || strcmp(fieldData, "hzl") == 0
+             || strcmp(fieldData, "oth") == 0);
 
         return temp;           
     } else if (strcmp(fieldName, "pid") == 0) 
-        return (strlen(fData) == 9 ? 1 : 0);
+        return (strlen(fieldData) == 9 ? 1 : 0);
     else
         return 0;
 }
@@ -80,7 +75,6 @@ int checkValidID(char** fields) {
 
     while (fgets(buffer, sizeof(buffer), f) != NULL) {       
         if (strcmp(buffer, "\n") == 0) {
-            printf("\n\n");
             temp = 0;
 
             for (i = 0; i < (NR_FIELDS + 1); i++)
@@ -104,7 +98,6 @@ int checkValidID(char** fields) {
                 token = strtok(NULL, " ");
 
                 if (!checkField(curFields[i], token)) {
-                    printf("\nDeu zika: %s - %s\n", curFields[i], token);
                     strcpy(curFields[i], "INVALID");
                     break;
                 }
